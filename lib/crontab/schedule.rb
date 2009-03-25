3119edbd219c731af79d019dfe59c2aa39bbc799
OZAWA Sakuro <github@2238club.org>
2009-03-25 18:34:33 +0900

    Import to git.

---
+++ lib/crontab/schedule.rb
@@ -0,0 +1,218 @@
+require 'date'
+
+module Crontab
+
+  class Schedule
+
+    MONTH_NAMES = %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+    DAY_OF_WEEK_NAMES = %w(Sun Mon Tue Wed Thu Fri Sat)
+    DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def initialize(spec, start=Time.now)
+      raise ArgumentError, 'empty spec' if spec == '' or spec.nil?
+      @start = ensure_time(start)
+      if spec.start_with?('@')
+        parse_symbolic_spec(spec)
+      else
+        parse_spec(spec)
+      end
+    end
+
+    def ==(other)
+      self.minutes == other.minutes &&
+      self.hours == other.hours &&
+      self.day_of_months == other.day_of_months &&
+      self.months == other.months &&
+      self.day_of_months_given? == other.day_of_months_given? &&
+      self.day_of_weeks == other.day_of_weeks &&
+      self.day_of_weeks_given? == other.day_of_weeks_given? &&
+      self.start == other.start
+    end
+
+    def hash
+      [ minutes, hours, day_of_months, months, day_of_weeks, day_of_months_given?, day_of_weeks_given?, start ].map(&:hash).inject(:^)
+    end
+
+    attr_reader :minutes, :hours, :day_of_months, :months, :day_of_weeks, :start
+
+    def day_of_months_given?
+      !!@day_of_months_given # ensures boolean
+    end
+    protected :day_of_months_given?
+
+    def day_of_weeks_given?
+      !!@day_of_weeks_given # ensures boolean
+    end
+    protected :day_of_weeks_given?
+
+    def each
+      return to_enum unless block_given?
+      year = @start.year
+      seeking = Hash.new {|h,k| h[k] = true }
+      loop do
+        @months.each do |month|
+          next if seeking[:month] and month < @start.month and @start.month <= @months.max
+          seeking[:month] = false
+          days = matching_days(year, month)
+          days.each do |day_of_month|
+            next if seeking[:day_of_month] and day_of_month < @start.day and @start.day <= days.max
+            seeking[:day_of_month] = false
+            @hours.each do |hour|
+              next if seeking[:hour] and hour < @start.hour and @start.hour <= @hours.max
+              seeking[:hour] = false
+              @minutes.each do |minute|
+                begin
+                  t = Time.local(year, month, day_of_month, hour, minute)
+                rescue ArgumentError
+                  raise StopIteration
+                end
+                yield(t) if @start <= t
+              end
+            end
+          end
+        end
+        year += 1
+      end
+    end
+
+    include Enumerable
+
+    def until(time_or_date)
+      time = ensure_time(time_or_date)
+      if block_given?
+        each do |t|
+          break if time < t
+          yield(t)
+        end
+      else
+        inject([]) do |timings, t|
+          break timings if time < t
+          timings << t
+        end
+      end
+    end
+
+    private
+
+    def parse_spec(spec)
+      args = spec.split(/\s+/)
+      raise ArgumentError, 'wrong number of spec fields: %d' % args.size unless args.size == 5
+      @minutes = parse_spec_field(args[0], 0..59, :accept_name => false)
+      @hours = parse_spec_field(args[1], 0..23, :accept_name => false)
+      @day_of_months = parse_spec_field(args[2], 1..31, :accept_name => false)
+      @day_of_months_given = args[2] != '*'
+      @months = parse_spec_field(args[3], 1..12, :names => MONTH_NAMES, :base => 1, :accept_name => true)
+      @day_of_weeks = parse_spec_field(args[4], 0..6, :names => DAY_OF_WEEK_NAMES, :base => 0, :accept_name => true, :allow_end => true)
+      @day_of_weeks_given = args[4] != '*'
+    end
+
+    def parse_symbolic_spec(spec)
+      case spec
+      when '@yearly', '@annually'
+        parse_spec('0 0 1 1 *')
+      when '@monthly'
+        parse_spec('0 0 1 * *')
+      when '@weekly'
+        parse_spec('0 0 * * 0')
+      when '@daily', '@midnight'
+        parse_spec('0 0 * * *')
+      when '@hourly'
+        parse_spec('0 * * * *')
+      when '@reboot'
+        raise NotImplementedError, '@reboot is not supported'
+      else
+        raise ArgumentError, 'unknown crontab spec: %s' % spec
+      end
+    end
+
+    def parse_spec_field(spec, range, options={})
+      options = { :accept_name => true, :allow_end => false }.merge(options)
+      case spec
+      when '*'
+        range.to_a
+      when /\A\d+\z/
+        [ parse_number(spec, range, options) ]
+      when /\A[a-z]{3}\z/i
+        [ parse_name(spec, range, options) ]
+      when /\A(\d+)-(\d+)\z/i
+        parse_range($1, $2, range, options.merge(:accept_name => false))
+      when /,/
+        parse_list(spec.split(/,/), range, options.merge(:accept_name => false))
+      when /\A(\*|\d+-\d+)\/(\d+)\z/
+        parse_step($1, $2.to_i, range, options.merge(:accept_name => false))
+      else
+        raise ArgumentError, 'wrong spec format: %s' % spec
+      end.tap do |result|
+        if options[:allow_end] && result.include?(range.first) && result.include?(range.last.succ)
+          result.delete(range.last.succ)
+        end
+      end.sort.uniq.freeze
+    end
+
+    def parse_number(spec, range, options)
+      v = spec.to_i
+      return v if range.include?(v) or options[:allow_end] && range.last.succ == v
+      raise ArgumentError, 'argument out of range: %s' % spec
+    end
+
+    def parse_name(spec, range, options)
+      raise ArgumentError, 'names not allowed in this field: %s' % spec unless options[:names] and options[:base]
+      raise ArgumentError, 'names not allowed in this context: %s' % spec unless options[:accept_name]
+      v = options[:names].index {|name| name.downcase == spec.downcase }
+      base = options[:base]
+      return v + base if v && range.include?(v + base)
+      raise ArgumentError, 'argument out of range: %s' % spec
+    end
+
+    def parse_range(from, to, range, options)
+      from = parse_number(from, range, options[:allow_end])
+      to = parse_number(to, range, options[:allow_end])
+      raise ArgumentError, 'start is after or equal to end: %s' % spec unless from < to
+      (from..to).to_a
+    end
+
+    def parse_list(specs, range, options)
+      specs.map{|spec| parse_spec_field(spec, range, options) }.flatten
+    end
+
+    def parse_step(first, step, range, options)
+      v = parse_spec_field(first, range, options)
+      v.first.step(v.size == 1 ? range.last : v.last, step).to_a
+    end
+
+    def matching_days(year, month)
+      days = number_of_days(year, month)
+      (1..days).select do |day|
+        wday = day_of_week(year, month, day)
+        if day_of_months_given?
+          if day_of_weeks_given?
+            @day_of_months.include?(day) or @day_of_weeks.include?(wday)
+          else
+            @day_of_months.include?(day)
+          end
+        else
+          if day_of_weeks_given?
+            @day_of_weeks.include?(wday)
+          else
+            true
+          end
+        end
+      end
+    end
+
+    def ensure_time(time_or_date)
+      time_or_date.is_a?(Date) ? Time.local(time_or_date.year, time_or_date.month, time_or_date.day) : time_or_date
+    end
+
+    def number_of_days(year, month)
+      days = DAYS_IN_MONTH[month]
+      days += 1 if month == 2 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+      days
+    end
+
+    def day_of_week(year, month, day)
+      Date.new(year, month, day).wday
+    end
+  end
+
+end
---
+++ spec/crontab/schedule_spec.rb
@@ -0,0 +1,266 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require 'crontab/schedule'
+
+describe Crontab::Schedule do
+  describe 'when parsing spec' do
+    it 'should accept spec with asterisks' do
+      schedule = Crontab::Schedule.new('* * * * *')
+      schedule.minutes.should == (0..59).to_a
+      schedule.hours.should == (0..23).to_a
+      schedule.day_of_months.should == (1..31).to_a
+      schedule.months.should == (1..12).to_a
+      schedule.day_of_weeks.should == (0..6).to_a
+    end
+
+    it 'should accept spec with simple numbers' do
+      schedule = Crontab::Schedule.new('0 0 1 1 0')
+      schedule.minutes.should == [0]
+      schedule.hours.should == [0]
+      schedule.day_of_months.should == [1]
+      schedule.months.should == [1]
+      schedule.day_of_weeks.should == [0]
+    end
+
+    it 'should accept spec with names(Jan, Sun etc.)' do
+      schedule = Crontab::Schedule.new('0 0 1 Jan sUn')
+      schedule.minutes.should == [0]
+      schedule.hours.should == [0]
+      schedule.day_of_months.should == [1]
+      schedule.months.should == [1]
+      schedule.day_of_weeks.should == [0]
+    end
+
+    it 'should accept spec with ranges(N-N)' do
+      schedule = Crontab::Schedule.new('0-1 0-1 1-2 1-2 0-1')
+      schedule.minutes.should == [0, 1]
+      schedule.hours.should == [0, 1]
+      schedule.day_of_months.should == [1, 2]
+      schedule.months.should == [1, 2]
+      schedule.day_of_weeks.should == [0, 1]
+    end
+
+    it 'should accept spec with lists(comma separaged ranges/numbers)' do
+      schedule = Crontab::Schedule.new('0,10,20 0,10,20 1,11,21 1,3,5 0,4,5,6')
+      schedule.minutes.should == [0, 10, 20]
+      schedule.hours.should == [0, 10, 20]
+      schedule.day_of_months.should == [1, 11, 21]
+      schedule.months.should == [1, 3, 5]
+      schedule.day_of_weeks.should == [0, 4, 5, 6]
+
+      schedule = Crontab::Schedule.new('0,1-5,10 0,1-5,10 1,2-5,10 1,2-4,10 0,1-3,7')
+      schedule.minutes.should == [0, 1, 2, 3, 4, 5, 10]
+      schedule.hours.should == [0, 1, 2, 3, 4, 5, 10]
+      schedule.day_of_months.should == [1, 2, 3, 4, 5, 10]
+      schedule.months.should == [1, 2, 3, 4, 10]
+      schedule.day_of_weeks.should == [0, 1, 2, 3]
+    end
+
+    it 'should accept spec with steps(range/STEP or */STEP)' do
+      schedule = Crontab::Schedule.new('0-30/10 1-20/5 5-30/7 4-10/2 1-5/2')
+      schedule.minutes.should == [0, 10, 20, 30]
+      schedule.hours.should == [1, 6, 11, 16 ]
+      schedule.day_of_months.should == [5, 12, 19, 26 ]
+      schedule.months.should == [4, 6, 8, 10]
+      schedule.day_of_weeks.should == [1, 3, 5]
+
+      schedule = Crontab::Schedule.new('*/25 */5 */10 */4 */2')
+      schedule.minutes.should == [0, 25, 50]
+      schedule.hours.should == [0, 5, 10, 15, 20]
+      schedule.day_of_months.should == [1, 11, 21, 31]
+      schedule.months.should == [1, 5, 9]
+      schedule.day_of_weeks.should == [0, 2, 4, 6]
+    end
+
+    it 'should reject spec with incorrect number of fields' do
+      lambda { Crontab::Schedule.new }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new(nil) }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('x') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('* * * *') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('* * * * * *') }.should raise_error(ArgumentError)
+    end
+
+    it 'should reject spec with incorrect values' do
+      lambda { Crontab::Schedule.new('x 0 1 0 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('-1 0 1 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('60 0 1 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 24 1 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 32 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 13 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 1 8') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 Mon 8') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('Jan Sun 1 1 8') }.should raise_error(ArgumentError)
+    end
+
+    it 'should reject spec with names in lists and ranges' do
+      lambda { Crontab::Schedule.new('0 0 1 Jan-Feb *') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 Jan,Feb *') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 * Sun-Fri') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 * Sun,Fri') }.should raise_error(ArgumentError)
+    end
+
+    it 'should reject spec with simple numbers with step' do
+      lambda { Crontab::Schedule.new('0/2 0 1 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0/2 1 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1/2 1 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 1/2 0') }.should raise_error(ArgumentError)
+      lambda { Crontab::Schedule.new('0 0 1 1 0/2') }.should raise_error(ArgumentError)
+    end
+
+    it 'should accept symblic specs' do
+      start = Time.now
+      Crontab::Schedule.new('@yearly', start).should == Crontab::Schedule.new('0 0 1 1 *', start)
+      Crontab::Schedule.new('@annually', start).should == Crontab::Schedule.new('0 0 1 1 *', start)
+      Crontab::Schedule.new('@monthly', start).should == Crontab::Schedule.new('0 0 1 * *', start)
+      Crontab::Schedule.new('@weekly', start).should == Crontab::Schedule.new('0 0 * * 0', start)
+      Crontab::Schedule.new('@daily', start).should == Crontab::Schedule.new('0 0 * * *', start)
+      Crontab::Schedule.new('@midnight', start).should == Crontab::Schedule.new('0 0 * * *', start)
+      Crontab::Schedule.new('@hourly', start).should == Crontab::Schedule.new('0 * * * *', start)
+    end
+
+    it 'should reject @reboot' do
+      lambda { Crontab::Schedule.new('@reboot') }.should raise_error(NotImplementedError)
+    end
+
+    it 'should reject unknown symbolic spec' do
+      lambda { Crontab::Schedule.new('@unknown') }.should raise_error(ArgumentError)
+    end
+
+  end
+
+  describe 'when comparing' do
+    it 'should equal to other if and only if both are created from equivalent spec and start time' do
+      start = Time.now
+      Crontab::Schedule.new('0 0 1 1 0', start).should == Crontab::Schedule.new('0 0 1 1 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should == Crontab::Schedule.new('0 0 1 Jan 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should == Crontab::Schedule.new('0 0 1 1 Sun', start)
+
+      Crontab::Schedule.new('0 0 1 1 0', start).should_not == Crontab::Schedule.new('1 0 1 1 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should_not == Crontab::Schedule.new('0 1 1 1 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should_not == Crontab::Schedule.new('0 0 2 1 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should_not == Crontab::Schedule.new('0 0 1 2 0', start)
+      Crontab::Schedule.new('0 0 1 1 0', start).should_not == Crontab::Schedule.new('0 0 1 1 1', start)
+
+      Crontab::Schedule.new('0 0 1-5 1 0', start).should == Crontab::Schedule.new('0 0 1,2,3,4,5 1 0', start)
+      Crontab::Schedule.new('0 0 1 1 0-4', start).should == Crontab::Schedule.new('0 0 1 1 0,1,2,3,4', start)
+    end
+  end
+
+  describe 'when enumerating' do
+    it 'should return given number of timings' do
+      schedule = Crontab::Schedule.new('* * * * *', Time.local(2009, 3, 8, 4, 30, 15))
+      expected = [
+        Time.local(2009, 3, 8, 4, 31),
+        Time.local(2009, 3, 8, 4, 32),
+        Time.local(2009, 3, 8, 4, 33),
+        Time.local(2009, 3, 8, 4, 34),
+        Time.local(2009, 3, 8, 4, 35),
+      ]
+      schedule.first(expected.size).should == expected
+
+      schedule = Crontab::Schedule.new('5 10 3-5,10 * *', Time.local(2009, 3, 8, 10, 30, 15))
+      expected = [
+        Time.local(2009, 3, 10, 10, 5),
+        Time.local(2009, 4,  3, 10, 5),
+        Time.local(2009, 4,  4, 10, 5),
+        Time.local(2009, 4,  5, 10, 5),
+        Time.local(2009, 4, 10, 10, 5),
+        Time.local(2009, 5,  3, 10, 5),
+        Time.local(2009, 5,  4, 10, 5),
+        Time.local(2009, 5,  5, 10, 5),
+        Time.local(2009, 5, 10, 10, 5),
+        Time.local(2009, 6,  3, 10, 5),
+      ]
+      schedule.first(expected.size).should == expected
+    end
+
+    it 'should return timings matching day of month or day of week' do
+      schedule = Crontab::Schedule.new('0 0 1,10,11 3 0,1', Time.local(2009, 3, 8, 10, 30, 15))
+      expected = [
+        Time.local(2009, 3,  9, 0, 0),
+        Time.local(2009, 3, 10, 0, 0),
+        Time.local(2009, 3, 11, 0, 0),
+        Time.local(2009, 3, 15, 0, 0),
+        Time.local(2009, 3, 16, 0, 0),
+        Time.local(2009, 3, 22, 0, 0),
+        Time.local(2009, 3, 23, 0, 0),
+        Time.local(2009, 3, 29, 0, 0),
+        Time.local(2009, 3, 30, 0, 0),
+      ]
+      schedule.first(expected.size).should == expected
+
+      schedule = Crontab::Schedule.new('0 0 1,10,11 3 *', Time.local(2009, 3, 8, 10, 30, 15))
+      expected = [
+        Time.local(2009, 3, 10, 0, 0),
+        Time.local(2009, 3, 11, 0, 0),
+      ]
+      schedule.first(expected.size).should == expected
+
+      schedule = Crontab::Schedule.new('0 0 * 3 0,1', Time.local(2009, 3, 8, 10, 30, 15))
+      expected = [
+        Time.local(2009, 3,  9, 0, 0),
+        Time.local(2009, 3, 15, 0, 0),
+        Time.local(2009, 3, 16, 0, 0),
+        Time.local(2009, 3, 22, 0, 0),
+        Time.local(2009, 3, 23, 0, 0),
+        Time.local(2009, 3, 29, 0, 0),
+        Time.local(2009, 3, 30, 0, 0),
+      ]
+      schedule.first(expected.size).should == expected
+    end
+
+    it 'should return timings until given time' do
+      schedule = Crontab::Schedule.new('* * * * *', Time.local(2009, 3, 8, 4, 30, 15))
+      expected = [
+        Time.local(2009, 3, 8, 4, 31),
+        Time.local(2009, 3, 8, 4, 32),
+        Time.local(2009, 3, 8, 4, 33),
+        Time.local(2009, 3, 8, 4, 34),
+        Time.local(2009, 3, 8, 4, 35),
+        Time.local(2009, 3, 8, 4, 36),
+        Time.local(2009, 3, 8, 4, 37),
+        Time.local(2009, 3, 8, 4, 38),
+        Time.local(2009, 3, 8, 4, 39),
+        Time.local(2009, 3, 8, 4, 40),
+      ]
+      schedule.until(Time.local(2009, 3, 8, 4, 40, 10)).should == expected
+
+      result = []
+      schedule.until(Time.local(2009, 3, 8, 4, 40, 10)) do |t|
+        result << t
+      end
+      result.should == expected
+
+      schedule = Crontab::Schedule.new('*/20 0 21 * *', Time.local(2009, 3, 8, 4, 30, 15))
+      expected = [
+        Time.local(2009, 3, 21, 0,  0),
+        Time.local(2009, 3, 21, 0, 20),
+        Time.local(2009, 3, 21, 0, 40),
+        Time.local(2009, 4, 21, 0,  0),
+        Time.local(2009, 4, 21, 0, 20),
+        Time.local(2009, 4, 21, 0, 40),
+        Time.local(2009, 5, 21, 0, 00),
+        Time.local(2009, 5, 21, 0, 20),
+        Time.local(2009, 5, 21, 0, 40),
+      ]
+      schedule.until(Time.local(2009, 6, 1)).should == expected
+    end
+
+    it 'should exclude invalid day of month' do
+      schedule = Crontab::Schedule.new('5 0 27,29,31 * *', Time.local(2009, 1, 1, 10, 30, 15))
+      expected = [
+        Time.local(2009, 1, 27, 0, 5),
+        Time.local(2009, 1, 29, 0, 5),
+        Time.local(2009, 1, 31, 0, 5),
+        Time.local(2009, 2, 27, 0, 5),
+        # Time.local(2009, 2, 29, 0, 5), # should exclude this!
+        # Time.local(2009, 2, 31, 0, 5), # should exclude this!
+        Time.local(2009, 3, 27, 0, 5),
+        Time.local(2009, 3, 29, 0, 5),
+        Time.local(2009, 3, 31, 0, 5),
+      ]
+
+      schedule.first(expected.size).should == expected
+    end
+  end
+end
