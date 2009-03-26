8510e732efa523423904f6e9246f69c46864e9d3
OZAWA Sakuro <github@2238club.org>
2009-03-26 19:21:39 +0900

    Implement Crontab class.

--- lib/crontab.rb
+++ lib/crontab.rb
@@ -1,2 +1,30 @@
 require 'crontab/schedule'
 require 'crontab/entry'
+
+class Crontab
+  class << self
+    def parse(src)
+      entries = []
+      env = {}
+      src.lines.each do |line|
+        line.strip!
+        case line
+        when /\A[\d*]/
+          entries << Crontab::Entry.parse(line)
+        when /\A([A-Z][A-Z0-9_]*)\s*=\s*/, /\A"([A-Z][A-Z0-9_]*)"\s*=\s*/, /\A'([A-Z][A-Z0-9_]*)'\s*=\s*/
+          name = $1
+          value = $'
+          value = $1 if /\A'(.*)'\z/ =~ value or /\A"(.*)"\z/ =~ value
+          env[name] = value
+        end
+      end
+      new(entries, env)
+    end
+  end
+
+  def initialize(entries, env)
+    @entries = entries.dup.freeze
+    @env = env.dup.freeze
+  end
+  attr_reader :entries, :env
+end
